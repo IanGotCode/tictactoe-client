d43a4b461479e48aec6fa746a828cce33a756070
IanGotCode <ian.gibson@iangibsonc.com>
2018-11-01 14:29:20 -0400

    redid the tic tac toe box, styled it, and started js logic

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,6 +6,20 @@
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
+const currentPlayer = 'X'
+const playerTwo = 'O'
+
 $(() => {
+  // all the ways the game can be won
+  const waysToWin = [
+    ['0', '1', '2'],
+    ['3', '4', '5'],
+    ['6', '7', '8'],
+    ['0', '4', '8'],
+    ['6', '4', '2'],
+    ['0', '3', '6'],
+    ['1', '4', '7'],
+    ['2', '5', '8']
+  ]
   // your JS code goes here
 })
