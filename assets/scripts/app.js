adb571eee5f576c47c105e6f1e195626f61a5300
IanGotCode <ian.gibson@iangibsonc.com>
2018-11-02 16:23:02 -0400

    updates to files added javascript to app.js making board functional switching between x's and o's

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -11,26 +11,48 @@ const playerOne = 'X'
 const playerTwo = 'O'
 
 let currentTurn = 1
-let movesMade = 0
+const movesMade = 0
 
-let box = $('.box')
+const gameBoard = ['', '', '', '', '', '', '', '', '']
 
-// click function that shows whats happening
-box.on('click', function (e) {
-  movesMade += 1
+$(document).ready(function () {
+  // combinations to actuually win the game
+  const waysToWin = [
+    ['0', '1', '2'],
+    ['3', '4', '5'],
+    ['6', '7', '8'],
+    ['0', '3', '6'],
+    ['1', '4', '7'],
+    ['2', '5', '8'],
+    ['0', '4', '8'],
+    ['6', '4', '2']
+  ]
 
-  if (currentTurn === 1) {
-    event.target.innerHTML = playerOne
-    event.target.style.color = 'white'
-    currentTurn += 1
-  } else {
-    event.target.innerHTML = playerTwo
-    event.target.style.color = 'white'
-    currentTurn -= 1
-  }
-})
+  // click function to allow X's to be clicked into squares
+  $('.box').click(function (event) {
+    const box = $(event.target)
+    if (currentTurn % 2 === 1) {
+      box.html(playerOne)
+      currentTurn++
+    } else if (currentTurn % 2 === 0) {
+      box.html(playerTwo)
+      currentTurn++
+    }
+    // assign values to the gameBoard array
 
-$(() => {
+    // const y = this.data('y')
+    // const x = this.data('x')
+    // if ()
+    // waysToWin[y][x] = playerOne // creates x to populate array in varible waysToWin in console.log
+    // console.log(waysToWin)
+  })
 
-  // your JS code goes here
+  // $('.box').click(function () {
+  //   const $this = $(this)
+  //   $this.html(playerTwo)
+  //   const y = $this.data('y')
+  //   const x = $this.data('x')
+  //   waysToWin[y][x] = playerTwo // creates x to populate array in varible waysToWin
+  //   console.log(waysToWin)
+  // })
 })
