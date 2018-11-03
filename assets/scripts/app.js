f64c9e5583550f646014b95df594fa2e3affa5f3
IanGotCode <ian.gibson@iangibsonc.com>
2018-11-02 22:49:23 -0400

    forgot to push app.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -15,44 +15,57 @@ const movesMade = 0
 
 const gameBoard = ['', '', '', '', '', '', '', '', '']
 
+let gameOver = false
+
 $(document).ready(function () {
   // combinations to actuually win the game
   const waysToWin = [
-    ['0', '1', '2'],
-    ['3', '4', '5'],
-    ['6', '7', '8'],
-    ['0', '3', '6'],
-    ['1', '4', '7'],
-    ['2', '5', '8'],
-    ['0', '4', '8'],
-    ['6', '4', '2']
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [6, 4, 2]
   ]
 
-  // click function to allow X's to be clicked into squares
+  // .click function allows clicks between x's and o's from class '.box'
   $('.box').click(function (event) {
     const box = $(event.target)
-    if (currentTurn % 2 === 1) {
-      box.html(playerOne)
-      currentTurn++
-    } else if (currentTurn % 2 === 0) {
-      box.html(playerTwo)
-      currentTurn++
+    // check to see if there is a value is in any of the boxes.
+    if (gameOver === false) {
+      if (box.text() === '') {
+        if (currentTurn % 2 === 1) {
+          box.html(playerOne)
+          // x's being added to gameBoard array
+          gameBoard[event.currentTarget.id] = playerOne
+          console.log(gameBoard)
+          checkForWinner()
+          currentTurn++
+        } else if (currentTurn % 2 === 0) {
+          box.html(playerTwo)
+          // y's being added to gameBoard array
+          gameBoard[event.currentTarget.id] = playerTwo
+          console.log(gameBoard)
+          checkForWinner()
+          currentTurn++
+        }
+      }
     }
-    // assign values to the gameBoard array
-
-    // const y = this.data('y')
-    // const x = this.data('x')
-    // if ()
-    // waysToWin[y][x] = playerOne // creates x to populate array in varible waysToWin in console.log
-    // console.log(waysToWin)
   })
 
-  // $('.box').click(function () {
-  //   const $this = $(this)
-  //   $this.html(playerTwo)
-  //   const y = $this.data('y')
-  //   const x = $this.data('x')
-  //   waysToWin[y][x] = playerTwo // creates x to populate array in varible waysToWin
-  //   console.log(waysToWin)
-  // })
+  // checking for winner
+  const checkForWinner = function () {
+    for (let i = 0; i < waysToWin.length; i++) {
+      if (gameBoard[waysToWin[i][0]] === playerOne && gameBoard[waysToWin[i][1]] === playerOne && gameBoard[waysToWin[i][2]] === playerOne) {
+        $('.modal').show()
+        gameOver = true
+      }
+      if (gameBoard[waysToWin[i][0]] === playerTwo && gameBoard[waysToWin[i][1]] === playerTwo && gameBoard[waysToWin[i][2]] === playerTwo) {
+        console.log('winner')
+        gameOver = true
+      }
+    }
+  }
 })
